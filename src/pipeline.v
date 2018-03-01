94e3136d573fa611432bd01037d83884f33d3b23
Evan Harrington <ewh010@bucknell.edu>
2018-03-01 14:03:07 -0500

    Some pipeline module

---
+++ src/pipeline.v
@@ -0,0 +1,29 @@
+//Group Name: 
+//March 1, 2018
+//This file will contain pipelining instructions: Fetch Decode Execute Mem Writeback
+
+
+/********************** FETCH MODULE ***************************/
+module Fetch(input [31:0] currPC, input [31:0] stall, output [31:0] PCF)
+
+endmodule
+
+/********************** DECODE MODULE ***************************/
+module Decode(input [31:0] instr, output [31:0] instrD)
+
+endmodule
+
+
+/********************** EXECUTE MODULE ***************************/
+module Execute()
+
+endmodule
+
+/********************** MEMORY MODULE ***************************/
+module Mem()
+
+endmodule
+
+/********************** WRITEBACK MODULE ***************************/
+module Writeback()
+endmodule
