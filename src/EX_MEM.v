cb54234d57a678313764064a5118ba7c7e8e0c13
Evan Harrington <ewh010@bucknell.edu>
2018-03-06 20:22:33 -0500

    ex_mem

--- src/EX_MEM.v
+++ src/EX_MEM.v
@@ -2,13 +2,13 @@
 // EX_MEM.v
 
 /* EX to MEM module */
-module EX_MEM(input clock, input [1:0] WB, input [2:0] MEMOut, input [4:0] regRD, input [31:0] ALUOut, input [31:0] WriteDataIn, output reg [1:0] WBreg, output reg [2:0] MEMreg, outputreg  [31:0] ALUreg, output reg [31:0] WriteDataOut, output reg  [4:0] RegRDreg)
+module EX_MEM(input clock, input [1:0] WB, input [2:0] MEMOut, input [4:0] regRD, input [31:0] ALUOut, input [31:0] WriteDataE, output reg [1:0] WBreg, output reg [2:0] MEMreg, outputreg  [31:0] ALUreg, output reg [31:0] WriteDataM, output reg  [4:0] RegRDreg)
 	initial
 	begin
 		WDreg = 0;
 		MEMreg = 0;
 		ALUreg = 0;
-		WriteDataOut = 0;
+		WriteDataM = 0;
 		RegRDreg = 0;
 	end
 
