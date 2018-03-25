c74f02cae1e4ae931b165fab3e4d60f470359cb0
Ryan Pencak <rvp002@bucknell.edu>
2018-03-25 12:54:04 -0400

    compile tests

--- test/add_test/add_test.s
+++ test/add_test/add_test.s
@@ -6,46 +6,11 @@
 
 __start:	        # define entry point for gcc
 li $t0, 1
-nop
-nop
-nop
-nop
-nop
 li $t1, 2
-nop
-nop
-nop
-nop
-nop
 
 li $v0, 1		# setup for print integer syscall
-nop
-nop
-nop
-nop
-nop
 add $a0, $t1, $t0	# syscall prints value in a0
-nop
-nop
-nop
-nop
-nop
 syscall
-nop
-nop
-nop
-nop
-nop
 
 li $v0, 10 		# exit syscall
-nop
-nop
-nop
-nop
-nop
 syscall
-nop
-nop
-nop
-nop
-nop
