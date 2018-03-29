07ba7c905a7c818671d20459d5b253e870c37da9
Evan Harrington <ewh010@bucknell.edu>
2018-03-29 17:17:38 -0400

    stuff

--- src/Control.v
+++ src/Control.v
@@ -5,7 +5,7 @@
 
 /* control module: determines control signal values */
 module Control(instr,
-               EX_D, MEM_D, WB_D, Jump, Branch, syscall_control, jr_control, jal_control, BranchOp/*, Byte_Warning*/);
+               EX_D, MEM_D, WB_D, Jump, Branch, syscall_control, jr_control, jal_control, BranchOp, Byte_Warning);
 
   /* declare inputs */
   input [31:0] instr;
@@ -20,7 +20,7 @@ module Control(instr,
   output reg syscall_control;
   output reg jr_control;
   output reg jal_control;
-  // output reg [1:0] Byte_Warning;
+  output reg [1:0] Byte_Warning;
 
   /* declare control signals */
   reg RegDst;
@@ -48,7 +48,7 @@ module Control(instr,
     EX_D = 0;
     MEM_D = 0;
     WB_D = 0;
-    // Byte_Warning = 2'b00;
+    Byte_Warning = 2'b00;
   end
 
   always @(instr) // case on instruction
@@ -143,7 +143,7 @@ module Control(instr,
           RegWrite = 1;
           ALUsrc = 1;
           ALUop = 5'b00010;
-          // Byte_Warning = 2'b00;
+          Byte_Warning = `SIZE_BYTE;
           $display("LW Instruction");
         end
 
@@ -152,7 +152,7 @@ module Control(instr,
           ALUop = 5'b00010;
           ALUsrc = 1;
           MemWrite = 1;
-          // Byte_Warning = 2'b01;
+          Byte_Warning = `SIZE_HALF;
           $display("SW Instruction");
         end
 
@@ -161,7 +161,7 @@ module Control(instr,
           ALUop = 5'b00010;
           ALUsrc = 1;
           MemWrite = 1;
-          // Byte_Warning = 2'b10;
+          Byte_Warning = `SIZE_WORD;
           $display("SB Instruction");
         end
 
--- src/Data_Memory.v
+++ src/Data_Memory.v
@@ -25,69 +25,65 @@ module Data_Memory(clk, memWrite, memRead, address, writeData/*, Byte_Warning*/,
       readData = memory[address >> 2]; // at read, set readData to memory at address shifted right 2
   end
 
-  always @(negedge clk)
-  begin
+  always @(negedge clk) begin
     if(memWrite == 1) begin
       memory[address >> 2] = writeData; // at write, set memory at address shifted right 2 to writeData
 
-  //     if(Byte_Warning == 0) begin
-  //       mem[address[31:2]] = writeData;
-  //
-  //     end
-  //
-  //     if(Byte_Warning == 1) begin
-  //       if(address[1] == 1) begin
-  //         mem[address[31:2]] = ((writeData << 16) & 32'hFFFF0000) | ((mem[address[31:2]) & 32'h0000FFFF);
-  //       end
-  //       if(address[1] == 0) begin
-  //         mem[address[31:2]] = ((writeData) & 32'h0000FFFF) | ((mem[address[31:2]) & 32'hFFFF0000);
-  //       end
-  //     end
-  //
-  //     if(Byte_Warning == 2) begin
-  //       if(address[1:0] == 3)begin
-  //         mem[address[31:2]] = ((writeData << 24) & 32'hFF000000) | ((mem[address[31:2]]) & 32'h00FFFFFF);
-  //       end
-  //       if(address[1:0] == 2) begin
-  //         mem[address[31:2]] = ((writeData << 16) & 32'h00FF0000) | ((mem[address[31:2]]) & 32'hFF00FFFF);
-  //       end
-  //       if(address[1:0] == 1) begin
-  //         mem[address[31:2]] = ((writeData << 8) & 32'h0000FF00) | ((mem[address[31:2]]) & 32'hFFFF00FF);
-  //       end
-  //       if(address[1:0] == 0) begin
-  //         mem[address[31:2]] = ((writeData) & 32'h000000FF) | ((mem[address[31:2]]) & 32'hFFFFFF00);
-  //       end
-  //     end
-  // end
-  //
-  //
-  //
-  //
-  // always @(address) begin
-  //   if (Byte_Warning == 0)begin
-  //     readData = mem[address[31:2]];
-  //   end
-  //
-  //   if (Byte_Warning == 1) begin
-  //     if (address[1] == 1)
-  //       readData = (mem[address[31:2]] & 32'hFFFF0000) >> 16;
-  //     if (address[1] ==0)
-  //       readData = (mem[address[31:2]] & 32'h0000FFFF);
-  //   end
-  //
-  //   if (Byte_Warning ==2) begin
-  //     if (address[1:0] == 3)
-  //       readData = (mem[address[31:2]] & 32'hFF000000) >> 24;
-  //     if (address[1:0] == 2)
-  //       readData = (mem[address[31:2]] & 32'h00FF0000) >> 16;
-  //     if (address[1:0] == 1)
-  //       readData = (mem[address[31:2]] & 32'h0000FF00) >> 8;
-  //     if (address[1:0] == 0)
-  //       readData = (mem[address[31:2]] & 32'h000000FF);
-  //   end
-  //   else begin
-  //     /////////////
+      if(Byte_Warning == `SIZE_BYTE) begin
+        mem[address[31:2]] = writeData;
+
+      end
+
+      if(Byte_Warning == `SIZE_HALF) begin
+        if(address[1] == 1) begin
+          mem[address[31:2]] = ((writeData << 16) & 32'hFFFF0000) | ((mem[address[31:2]) & 32'h0000FFFF);
+        end
+        if(address[1] == 0) begin
+          mem[address[31:2]] = ((writeData) & 32'h0000FFFF) | ((mem[address[31:2]) & 32'hFFFF0000);
+        end
+      end
+    
+      if(Byte_Warning == `SIZE_WORD) begin
+        if(address[1:0] == 3)begin
+          mem[address[31:2]] = ((writeData << 24) & 32'hFF000000) | ((mem[address[31:2]]) & 32'h00FFFFFF);
+        end
+        if(address[1:0] == 2) begin
+          mem[address[31:2]] = ((writeData << 16) & 32'h00FF0000) | ((mem[address[31:2]]) & 32'hFF00FFFF);
+        end
+        if(address[1:0] == 1) begin
+          mem[address[31:2]] = ((writeData << 8) & 32'h0000FF00) | ((mem[address[31:2]]) & 32'hFFFF00FF);
+        end
+        if(address[1:0] == 0) begin
+          mem[address[31:2]] = ((writeData) & 32'h000000FF) | ((mem[address[31:2]]) & 32'hFFFFFF00);
+        end
+      end
+  end
+
+  always @(address) begin      // whenever the address changes 
+    if (Byte_Warning == 0)begin
+      readData = mem[address[31:2]];
     end
+
+    if (Byte_Warning == 1) begin
+      if (address[1] == 1)
+        readData = (mem[address[31:2]] & 32'hFFFF0000) >> 16;
+      if (address[1] == 0)
+        readData = (mem[address[31:2]] & 32'h0000FFFF);
+    end
+
+    if (Byte_Warning ==2) begin
+      if (address[1:0] == 3)
+        readData = (mem[address[31:2]] & 32'hFF000000) >> 24;
+      if (address[1:0] == 2)
+        readData = (mem[address[31:2]] & 32'h00FF0000) >> 16;
+      if (address[1:0] == 1)
+        readData = (mem[address[31:2]] & 32'h0000FF00) >> 8;
+      if (address[1:0] == 0)
+       readData = (mem[address[31:2]] & 32'h000000FF);
+    end
+    else begin
+      if 
+end
 
   end
 endmodule
