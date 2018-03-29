c9099b34ca874953d9a9a1314de9600c0c1e9f36
Ryan Pencak <rvp002@bucknell.edu>
2018-03-29 12:15:11 -0400

    test

--- test/string_test/add_test.s
+++ test/string_test/add_test.s
@@ -1,14 +1,15 @@
 # This test program loads two immediate values
 # and prints the sum using a syscall.
 
-.global __start		# export start symbol
-.text
-
 .data
-     str1: .asciiz    “A”
-     str2: .asciiz    “Hello”
-.globl main
+     str: .asciiz    “Hello”
+     
+.text
 main:
-    lui $a0, $str2
-    addi $v0, $0, 4
+
+    la $a0, $str
+    li $v0, 4
+    syscall
+
+    li $v0, 10
     syscall
